ec651026e2118fc1b6c8779bfe39511584abc84d
Ang, Ting Soon <ting.soon.ang@accenture.com>
2020-08-24 11:23:03 +0800

    Added test del request in controller

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,19 +2,32 @@
 
 namespace Drupal\testapi\Controller;
 
+use GuzzleHttp\Exception\RequestException;
 use Symfony\Component\HttpFoundation\Response;
 
 class TestController {
 
   public function build() {
 
-    // Create a client with a base URI
+//    // Create a client with a base URI
+//    $client = \Drupal::httpClient();
+//    $request = $client->get('https://jsonplaceholder.typicode.com/todos/1');
+//    $response = $request->getBody()->getContents();
+//
+////    drupal_set_message($response);
+//
+//    return new Response($response);
+
+    // test delete request
     $client = \Drupal::httpClient();
-    $request = $client->get('https://jsonplaceholder.typicode.com/todos/1');
-    $response = $request->getBody()->getContents();
+    $delUrl = 'https://jsonplaceholder.typicode.com/todos/1';
 
-//    drupal_set_message($response);
-
-    return new Response($response);
+    try {
+      $response = $client->delete($delUrl);
+      $statusCode = $response->getStatusCode();
+    } catch (RequestException $e) {
+      return new Response($e->getMessage());
+    }
+      return new Response('Status Code: '.$statusCode);
   }
 }
